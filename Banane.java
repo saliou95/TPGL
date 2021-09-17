54346c06ed48681add39ad224c635ba9da47ffe3
saliou95 <dmamadousaliou288@gmail.com>
2021-09-17 14:34:29 +0200

    ajout fruit

---
+++ Banane.java
@@ -0,0 +1,69 @@
+/**
+ *
+ * @author roudet
+ */
+public class Banane {
+    private double prix;
+    private String origine;
+	
+	public static void main (String args[])
+	{
+		Banane or = new Banane(-12, "");
+		System.out.println("Prix : "+or.getPrix());
+		System.out.println("Origine : "+or.getOrigine());
+		System.out.println(or.toString());
+	}	
+	
+    public Banane() 
+    {
+        this.prix = 0.5;  //prix en euros
+        this.origine="Espagne";
+    }
+    
+    public Banane(double prix, String origine) 
+    {
+	if(prix < 0)
+	    this.prix = -prix;  //une solution possible pour interdire les prix negatifs
+	else
+	    this.prix = prix;
+
+	if(origine.equals(""))
+            this.origine = "Espagne";  //Espagne par défaut
+	else
+            this.origine = origine;   
+    }
+
+    public double getPrix(){
+	return prix;
+    }
+
+    public void setPrix(double prix){
+	this.prix=prix;
+    }
+
+    public String getOrigine(){
+	return origine;
+    }
+ 
+    public void setOrigine(String origine){
+	this.origine=origine;
+    }
+
+    @Override
+    public String toString(){
+        return "Banane de " + origine + " a " + prix + " euros";
+    }
+
+    @Override
+    public boolean equals(Object o){  //predicat pour tester si 2 Bananes sont equivalentes
+        if(o != null && getClass() == o.getClass()){
+            Banane or = (Banane) o;
+            return (prix == or.prix && origine.equals(or.origine));
+        }
+        return false;
+    }
+
+    public boolean isSeedless() {  //predicat indiquant qu'une Banane a des pepins
+        return false;
+    }
+}
